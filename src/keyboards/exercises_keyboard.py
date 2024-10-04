0dc7140ffd6f0b7af155044f4738559d266169d4
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-04 12:19:58 +0500

    Add func get_keyboard_with_exercises which create keyboard to select a workout

---
+++ src/keyboards/exercises_keyboard.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from src.database.exercises import exercises
+
+
+def get_keyboard_with_exercises() -> InlineKeyboardMarkup:
+    """The function returns the keyboard to select a workout"""
+    buttons: List[InlineKeyboardButton] = [
+        InlineKeyboardButton(
+            text=f"{exercise} - {price} руб.",
+            callback_data=exercise
+        )
+        for exercise, price in exercises.items()
+    ]
+    return InlineKeyboardMarkup(
+        inline_keyboard=[[button] for button in buttons]
+    )
